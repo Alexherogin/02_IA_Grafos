3e308a3c82aea6b02457e85123245d075d357e29
Alexherogin <ragna.no.gin@gmail.com>
2024-04-21 14:19:48 -0600

    UCS_v2
    se busco la solución para el problema de búsqueda en el grafo

--- 03_Busqueda_Costo_Uniforme_UCS.py
+++ 03_Busqueda_Costo_Uniforme_UCS.py
@@ -4,32 +4,39 @@ import heapq # para usar la cola de prioridad
 
 #Funcion para encontrar el camino mas corto
 
-def CostoU(greafo,Nodo_Star):
+def CostoU(grafo,Nodo_Star):
     visitados = set()
     #Cola de prioridad
-    cola = [(0,Nodo_Star,())]
+    cola = [(0,Nodo_Star,)]
     
     while cola:
-        (costo_acumulado,Nodo_actual,camino) = heapq.heappop(cola)
+        (costo_acumulado,Nodo_actual) = heapq.heappop(cola)
         if Nodo_actual in visitados:
             continue
 
         visitados.add(Nodo_actual)
-        print(Nodo_actual)
+        print(f'Paso de nodo:',{Nodo_actual},'Costo acumulado:',{costo_acumulado})
 
-        for (costo,Nodo_siguiente,direccion) in grafo[Nodo_actual]:
+        for Nodo_siguiente,costoT in grafo.get(Nodo_actual,[]):
             if Nodo_siguiente not in visitados:
-                Nuevo_costo = costo_acumulado + costo
-                heapq.heappush(cola,(Nuevo_costo,Nodo_siguiente,camino + (direccion,)))
-    return camino 
+                Nuevo_costo = costo_acumulado + int(costoT)
+                heapq.heappush(cola,(Nuevo_costo,Nodo_siguiente, ))
+    return visitados
 
-grafo ={
-    'entrada':[('sala',1),('cuarto_PB',1.5)],
-    'sala':[('comedor',2),('escaleras',1.8)],
-    'comedor':[('sala',2),('cocina',1.5)],
-    'cocina':[('comedor',1.5),('cuarto_PB',1.5)],
-    'cuarto_PB':[('cocina',1.5),('entrada',1.5),('escaleras',1.5)]
-
-    
-    }
+grafo = { 'entrada':[('sala',1),('cuarto_PB',1)],
+    'sala':[('comedor',2),('escaleras',2)],
+    'cuarto_PB':[],
+    'comedor':[('baño_PB',2),('cocina',4)],
+    'baño_PB':[],
+    'cocina':[('cuarto_lavado',5),('patio_Tracero',2)],
+    'cuarto_lavado':[],
+    'Patio_Tracero':[],
+    'escaleras':[('planta_Alta',3)],
+    'planta_Alta':[('Cuarto_PA_1',9),('vestibulo',8),('baño_PA',9),('cuarto_2_PA',11)],
+    'Cuarto_PA_1':[],
+    'vestibulo':[],
+    'baño_PA':[],
+    'cuarto_2_PA':[('baño_cuarto',11.),('valcon',12)],
+    'baño_cuarto':[],
+    'valcon':[],}
 CostoU(grafo,'entrada')
