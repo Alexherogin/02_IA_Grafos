1fb59b2bf9f57b9cde32031055b00d85fa677f11
Alexherogin <ragna.no.gin@gmail.com>
2024-05-03 23:23:35 -0600

    v2
    mejoras en el programa y restructuración del programa

--- Busqueda informada/09_Busqueda_por(A-star).py
+++ Busqueda informada/09_Busqueda_por(A-star).py
@@ -1,7 +1,7 @@
 #busqueda por A* y AO*
 import heapq
 
-def A_star(grafo,inicio,meta): 
+def A_star(grafo,inicio,meta): #
     open_set=[]
     heapq.heappush(open_set,(0,inicio)) 
 
@@ -39,33 +39,28 @@ def reconstruir_camino(camino,nodo_actual):
     return paso
 
 def AO_star(grafo,inicio,meta):
-    open_set=[0,inicio]
+    open_set=[]
+    heapq.heappush(open_set,(0,inicio))
     camino={}
     g={nodo:float('inf')for nodo in grafo}
-    g=[meta]=0
+    g=[inicio]=0
 
     while open_set: 
-        _,nodo_actual= open_set.pop(0)
+        _,nodo_actual= heapq.heappop(open_set)
         if nodo_actual == meta:
-            return reconstruir_camino(camino,nodo_actual)
+            return reconstruir_camino(camino,meta)
         for vecino in grafo[nodo_actual]:
             nuevo_g= g [nodo_actual] + grafo[nodo_actual][vecino]
             if nuevo_g<g[vecino]:
-                camino[vecino] = nuevo_g
+                camino[vecino] = nodo_actual
+                g[vecino]=nuevo_g
                 f=g[vecino]+heuristica(vecino,meta)
+                heapq.heappush(open_set,(f,vecino))
                 open_set.append((f,vecino))
                 open_set.sort(key=lambda x:[0])
     return None
-def heuristica(nodo,meta):
-    x1,y1=nodo
-    x2,y2=meta
-def reconstruir_camino(camino,nodo_actual):
-    paso=[nodo_actual]
-    while nodo_actual in camino:
-        nodo_actual=camino[nodo_actual]
-        paso.append(nodo_actual)
-    paso.reverse()
-    return paso
+
+
 
 
             
