6feae9b4b38b14f1054d1dbe5bc73640dee15eb9
Alexherogin <ragna.no.gin@gmail.com>
2024-05-05 19:49:43 -0600

    interacion valores
    se genero un programa pequeño para la interacion de valores

---
+++ utilidad_y_toma_deciciones/26_ineracion_valores.py
@@ -0,0 +1,13 @@
+#
+
+
+a=int(input('ingrese el primer valor: '))
+b=int(input('ingrese el segundo valor: '))
+c=int(input('ingrese el tercero valor: '))
+d=int(input('ingrese el cuarto valor: '))
+e=int(input('ingrese el quinto valor: '))
+
+valores =[a,b,c,d,e]
+for valor in valores:
+    print(valor)
+    
